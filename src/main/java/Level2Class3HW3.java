415061a56ef253b7c21474382fb71bf56297f76d
Vlad Lehtikov <vlad.lekhtsikau@gmail.com>
2017-08-02 21:59:22 -0700

    Level 2 Class 3 Third HW Task

---
+++ src/main/java/Level2Class3HW3.java
@@ -0,0 +1,23 @@
+
+
+import java.util.HashSet;
+
+public class Level2Class3HW3 {
+
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<String>();
+		set.add("One");
+		set.add("Two");
+		set.add("One");
+		set.add("Three");
+		set.add("Two");
+		set.add("Four");
+		set.add("One");
+		set.add("Three");
+		set.add("Five");
+		
+		System.out.println(set);
+
+	}
+
+}
