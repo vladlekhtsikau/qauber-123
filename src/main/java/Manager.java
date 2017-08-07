a4676e52f821f3aa6b38c96b2d3382dca2906a13
Vlad Lehtikov <vlad.lekhtsikau@gmail.com>
2017-08-06 21:10:44 -0700

    Level 2 Class 4 Homework

---
+++ src/main/java/Manager.java
@@ -0,0 +1,15 @@
+
+
+public class Manager extends Employee {
+	
+		public Manager(String name, int salary){
+			super(name, salary);
+		}
+			
+		public void calculateSalary(){
+			int managerSalary = salary + 5000;
+			int salary = managerSalary;
+		}
+		
+			
+		}
