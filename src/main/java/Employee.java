c7c795f11086ae7593ca9ddae6e7b76d9ba24d25
Vlad Lehtikov <vlad.lekhtsikau@gmail.com>
2017-08-06 21:09:13 -0700

    Level 2 Class 4 Homework

---
+++ src/main/java/Employee.java
@@ -0,0 +1,18 @@
+
+
+public class Employee {
+	
+		protected String name;
+		protected int salary;
+		
+		public Employee(String name,int salary){
+			this.name = name;
+			this.salary = salary;
+			
+		}
+		
+		public void calculateSalary(){
+			
+		}
+		
+}
