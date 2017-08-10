27cbce65adc859f83b392a9455228eea04c43c3f
Vlad Lehtikov <vlad.lekhtsikau@gmail.com>
2017-08-09 17:37:12 -0700

    Level 2 Class 5 Homework

---
+++ src/main/java/Level2Class5_1.java
@@ -0,0 +1,17 @@
+
+
+public class Level2Class5_1 {
+
+	public void getSomething() throws InsufficientBalanceException{
+		System.out.println("Hello I am trapped in a code ...");
+		
+		try {
+			
+		} catch (Exception s) {
+			
+			s.getClass();
+		}
+		
+	}
+
+}
